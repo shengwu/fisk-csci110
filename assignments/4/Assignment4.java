498ca68761c9245d8f950016e8cb364f17ef3b18
Sheng Wu <pkernoobie@gmail.com>
2016-09-27 08:38:24 -0500

    start assignment 4

---
+++ assignments/4/Assignment4.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Assignment 4 (100 points)
+ * Due: TODO
+ *
+ * Implement each of the following functions. Verify their correctness
+ * using the tests in Assignment4Test.java.
+ *
+ * In addition to the bonus questions, extra credit is available by adding
+ * tests: 1 point for each added test case up to 20 points.
+ *
+ * Please work with your assigned partner, but *do not* collaborate
+ * with anyone else whatsoever. You will probably do better if you do not
+ * split up the problems, but instead each do all of them and discuss.
+ */
+public class Assignment4 {
+    /**
+     * Getting started
+     * ---------------
+     *
+     * Say you had a problem like this:
+
+    public static ArrayList<Integer> getOneThruFive() {
+        // TODO: implement this.
+        return new ArrayList<Integer>();
+    }
+
+     * The documentation above the function says "Returns an ArrayList
+     * containing the numbers 1 through 5." To solve this problem, you would
+     * write something like this:
+
+    public static ArrayList<Integer> getOneThruFive() {
+        ArrayList<Integer> oneThruFive =  new ArrayList<Integer>();
+        oneThruFive.add(1);
+        oneThruFive.add(2);
+        oneThruFive.add(3);
+        oneThruFive.add(4);
+        oneThruFive.add(5);
+        return oneThruFive;
+    }
+
+     * After implementing the function, you would run the tests to make sure
+     * the tests for this function pass:
+     *
+     * javac Assignment4.java Assignment4Test.java
+     * java org.junit.runner.JUnitCore Assignment4Test
+     */
+
+
+    /**
+     * Problem 1 (10 points)
+     *
+     * Returns an ArrayList containing the first and seventh element in nums.
+     * Do not modify nums.
+     *
+     * For example, if nums is [1 2 3 4 5 6 7 8 9 10], then this function
+     * should return [1 7].
+     *
+     * If nums is [3], this function should return [3 0] since there is no
+     * seventh element.
+     */
+    public static ArrayList<Integer> getFirstAndSeventh(ArrayList<Integer> nums) {
+        // TODO: implement this.
+        return new ArrayList<Integer>();
+    }
+
+    /**
+     * Problem 2 (10 points)
+     *
+     * Returns an ArrayList containing every third element in nums. Do not
+     * modify nums.
+     *
+     * For example, if nums is [1 2 3 4 5 6 7 8 9 10], then this function
+     * should return [3 6 9].
+     */
+    public static ArrayList<Integer> getThirdElements(ArrayList<Integer> nums) {
+        // TODO: implement this.
+        return new ArrayList<Integer>();
+    }
+
+
+}
