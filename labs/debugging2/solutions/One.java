b6a1d4c30f9e7539d6e44e4b094d7795f52408a5
Sheng Wu <pkernoobie@gmail.com>
2016-10-11 19:10:19 -0700

    first part of debugging lab 2

--- labs/debugging2/solutions/One.java
+++ labs/debugging2/solutions/One.java
@@ -6,7 +6,8 @@
  */
 public class One {
 
-    static void printIfGreater(int i, int limit) {
+    // parameters should be of type double
+    static void printIfGreater(double i, double limit) {
         if (i > limit) {
             System.out.println(i + " is greater than " + limit);
         }
@@ -15,15 +16,18 @@ public class One {
     public static void main(String[] args) {
         double[] nums = {5.6, 1.0, 8.8, 2.5, 2.0, 10.432, 12.4, 98.2, 1.2, 91.2};
 
-        for (i = 0; i < nums.length; i++) {
+        // the first part needs the 'int' keyword to complete the variable declaration
+        for (int i = 0; i < nums.length; i++) {
             nums[i] = Math.pow(nums[i], 2);
         }
 
+        // math.sqrt only takes one parameter; removed the second
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = Math.sqrt(nums[i], 2);
+            nums[i] = Math.sqrt(nums[i]);
         }
 
-        for (int j < 0; j < nums.length; j++) {
+        // should be j = 0, not j < 0 in first part
+        for (int j = 0; j < nums.length; j++) {
             if (j < 4) {
                 nums[j] -= 1.0;
             }
@@ -31,16 +35,19 @@ public class One {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 5) {
-                nums[j] += 2.0;
+                // wrong variable: j instead of i
+                nums[i] += 2.0;
             }
         }
 
+        // multiply, divide shorthand should put the symbol before the
+        // assignment operator
         for (int i = 0; i < nums.length; i++) {
-            nums[i] =* 2;
+            nums[i] *= 2;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            nums[i] =/ 2;
+            nums[i] /= 2;
         }
 
         for (int i = 1; i < nums.length; i++) {
