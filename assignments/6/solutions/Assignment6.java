8dca029b97cf00369002a99141395d4712d585bc
Sheng Wu <pkernoobie@gmail.com>
2016-11-05 06:49:43 -0500

    full draft of assignment 6 and solutions

--- assignments/6/solutions/Assignment6.java
+++ assignments/6/solutions/Assignment6.java
@@ -26,8 +26,11 @@ public class Assignment6 {
      *     6: true
      */
     public HashMap<Integer, Boolean> enumerateDivisibleBy3() {
-        // TODO
-        return new HashMap<Integer, Boolean>();
+        HashMap<Integer, Boolean> result = new HashMap<Integer, Boolean>();
+        for (int i = 1; i <= 100; i++) {
+            result.put(i, i % 3 == 0);
+        }
+        return result;
     }
 
 
@@ -36,7 +39,8 @@ public class Assignment6 {
     /**
      * Problem 2: 10 points
      *
-     * Counts the occurrences of uppercase As, Bs, and Cs in 's'.
+     * Counts the occurrences of uppercase As, Bs, and Cs in 's'. The HashMap
+     * should not contain entries for any other letters.
      *
      * Example: if given the string "BALLOON", this function should return a
      * HashMap containing:
@@ -44,8 +48,19 @@ public class Assignment6 {
      *     B: 1
      */
     public HashMap<Character, Integer> countAsBsandCs(String s) {
-        // TODO
-        return new HashMap<Character, Integer>();
+        HashMap<Character, Integer> result = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'A' || c == 'B' || c == 'C') {
+                if (result.containsKey(c)) {
+                    result.put(c, result.get(c) + 1);
+                } else {
+                    // it wasn't already in there
+                    result.put(c, 1);
+                }
+            }
+        }
+        return result;
     }
 
 
@@ -53,7 +68,9 @@ public class Assignment6 {
     /**
      * Problem 3: 10 points
      *
-     * Returns true if 's' contains at least 10 distinct letters.
+     * Returns true if 's' contains at least 10 distinct letters. This method
+     * should be case-sensitive (i.e. uppercase letters are distinct from
+     * lowercase letters).
      *
      * For example, 'hellohello' would result in false because
      * it only has 4 distinct letters: h, e, l, and o
@@ -62,8 +79,12 @@ public class Assignment6 {
      * are in a HashMap?
      */
     public boolean hasAtLeast10Distinct(String s) {
-        // TODO
-        return false;
+        HashMap<Character, Boolean> result = new HashMap<Character, Boolean>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            result.put(c, true);
+        }
+        return result.keySet().size() >= 10;
     }
 
 
@@ -82,8 +103,30 @@ public class Assignment6 {
      * HashMap.
      */
     public String compressCoinTosses(String coinTosses) {
-        // TODO
-        return "";
+        if (coinTosses.length() == 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        char curr = coinTosses.charAt(0);
+        int currCount = 1;
+        for (int i = 1; i < coinTosses.length(); i++) {
+            char c = coinTosses.charAt(i);
+            if (c == curr) {
+                currCount++;
+            } else {
+                // we have a new letter!
+                // save the previous letter and its count to the result
+                result.append(curr);
+                result.append(currCount);
+                // reset curr and currCount since we have a new letter
+                curr = c;
+                currCount = 1;
+            }
+        }
+        // save whatever we were doing last
+        result.append(curr);
+        result.append(currCount);
+        return result.toString();
     }
 
 
@@ -102,8 +145,11 @@ public class Assignment6 {
      */
     public HashMap<String, String> createZipLookup(ArrayList<String> zipCodes,
             ArrayList<String> cities) {
-        // TODO
-        return new HashMap<String, String>();
+        HashMap<String, String> zipToCity = new HashMap<String, String>();
+        for (int i = 0; i < zipCodes.size(); i++) {
+            zipToCity.put(zipCodes.get(i), cities.get(i));
+        }
+        return zipToCity;
     }
 
 
@@ -128,8 +174,13 @@ public class Assignment6 {
      * http://docs.oracle.com/javase/7/docs/api/java/lang/String.html#split(java.lang.String)
      */
     public boolean allValidWords(String sentence, HashSet<String> dictionary) {
-        // TODO
-        return false;
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (!dictionary.contains(words[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -151,8 +202,11 @@ public class Assignment6 {
      */
     public HashMap<String, Integer> countCities(
             HashMap<String, ArrayList<String>> stateToCities) {
-        // TODO
-        return new HashMap<String, Integer>();
+        HashMap<String, Integer> cityCounts = new HashMap<String, Integer>();
+        for (String city : stateToCities.keySet()) {
+            cityCounts.put(city, stateToCities.get(city).size());
+        }
+        return cityCounts;
     }
 
 
@@ -179,8 +233,28 @@ public class Assignment6 {
      * Sorts an array of Dogs in descending order by tailWag. Uses selection sort
      * sort (no use of built-in or other prewritten sorting libraries allowed).
      */
+
+    void swap(Dog[] dogs, int a, int b) {
+        Dog tmp = dogs[a];
+        dogs[a] = dogs[b];
+        dogs[b] = tmp;
+    }
+
     public void sort(Dog[] dogs) {
-        // TODO
+        for (int i = 1; i < dogs.length; i++) {
+            Dog curr = dogs[i];
+            // Insert it in the appropriate place in the sorted portion
+            for (int j = i-1; j >= 0; j--) {
+                if (curr.getTailWag() > dogs[j].getTailWag()) {
+                    // If the one we're looking at 
+                    dogs[j+1] = dogs[j];
+                } else {
+                    // put the dog in the j+1st spot (where we just made room)
+                    dogs[j+1] = curr;
+                    break;
+                }
+            }
+        }
     }
 
 
@@ -205,8 +279,11 @@ public class Assignment6 {
      * a String like "abc" will hash to the same value as "cba".
      */
     public int badHash(String s, int mod) {
-        // TODO
-        return 0;
+        int acc = 1;
+        for (int i = 0; i < s.length(); i++) {
+            acc = (acc * s.charAt(i)) % mod;
+        }
+        return acc;
     }
 
 }
