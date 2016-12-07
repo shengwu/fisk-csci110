99d27ce776e37470f39a332e195a0373a9b6e136
Sheng Wu <pkernoobie@gmail.com>
2016-12-07 07:55:41 -0600

    update move_into_folders script

--- assignments/move_into_folders.sh
+++ assignments/move_into_folders.sh
@@ -2,10 +2,18 @@
 # Run in an unzipped pack of submissions downloaded from Canvas.
 # This will split each student's java files into a folder with his/her name.
 
-for filename in `ls *.java`
+# Sometimes there are spaces in filenames
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for filename in `ls`
 do
-    FOLDER=`echo $filename | awk -F _ '{print $1}'`
-    FILE=`echo $filename | awk -F _ '{print $4}'`
-    mkdir -p $FOLDER
-    mv $filename $FOLDER/$FILE
+    # A new filename with "late_" removed
+    sfilename=`echo $filename | sed s/late_//`
+    FOLDER=`echo $sfilename | awk -F _ '{print $1}'`
+    FILE=`echo $sfilename | awk -F _ '{print $4}'`
+    mkdir -p "$FOLDER"
+    mv "$filename" "$FOLDER/$FILE"
 done
+
+IFS=$SAVEIFS
